39cb0c7ac50e637adc6c6d77b1323bb0b8914c9a
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-11 13:52:05 +0530

    gamnling problem

--- src/GamblingProblem.java
+++ src/GamblingProblem.java
@@ -1,21 +1,39 @@
-import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 
 public class GamblingProblem {
     public static void main(String[] args) {
-        int startAmount = 100;
-        int bettingAmount = 1;
-        int count = 0;
-        while (startAmount > 0 && startAmount < 200) {
-            int chance = (int) (Math.random() * 10) % 2;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int loss = 0;
+        int win = 0;
+        for (int i = 0; i <= n; i++) {
+            int startAmount = 100;
+            int bettingAmount = 1;
+            int count = 0;
 
-            if (chance == 0) {
-                startAmount = startAmount - bettingAmount;
-            } else {
-                startAmount = startAmount + bettingAmount;
+            while (startAmount > 0 && startAmount < 200) {
+                int chance = (int)(Math.random() * 10) % 2;  // 0 or 1
+
+                if (chance == 0) {
+                    startAmount = startAmount - bettingAmount;
+                } else {
+                    startAmount = startAmount + bettingAmount;
+                }
+                count++;
             }
-            count++;
+            System.out.println(" ");
+            System.out.println("number of games player " + count);
+            System.out.println("Gambler amount " + startAmount);
+            if (startAmount == 0){
+                System.out.println("gambler lost");
+                loss++;
+            }else {
+                System.out.println("gambler won");
+                win++;
+            }
         }
-        System.out.println("number of games player "+ count);
+        System.out.println(" ");
+        System.out.println("gambler won "+ win);
+        System.out.println("gambler lose "+ loss);
     }
 }
-
